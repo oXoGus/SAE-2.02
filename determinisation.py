d60e8e13943200c63c671e00184ab7074ac222d3
oXoGus <mdintrat0205@gmail.com>
2025-02-16 00:19:58 +0100

    déterminisation finie

--- determinisation.py
+++ determinisation.py
@@ -42,15 +42,142 @@ def determinise(auto):
     if deterministe(auto):
         return auto
     
-    # I est la liste des états initiaux 
-    auto["I"] = [auto["I"]]
+    # on recréer un auto det
+    autoDet = {
+        'alphabet': auto['alphabet'],
+        'I': [],
+        'etats': [],
+        'transitions': [],
+        'F': []
+    }
 
-    # les transitions 
-    #     
+    # I est la liste des états initiaux 
+    autoDet["I"] = [auto["I"]]
+
+    # on part de l'état initial (la liste contenance tout les états initiaux)
+    # l'état actif est l'état d'où part les transitions
+    etatActif =  autoDet["I"][0]
+
+    # on met l'état actif dans la liste des états non marqué
+    # puisqu'on a pas encore test toute ces transitions
+    
+    # un état est marqué quand on a fait toutes ces transition possible
+
+    etatsNonMarque = [etatActif]
+
+    etatsMarque = []
+
+    # tant qu'il reste des états non marqué
+    while len(etatsNonMarque) > 0:
+
+        # on marque l'état active
+        # en verifiant tout ces transistions possibles
+
+        etatsTrouve = marquer(etatsNonMarque[0], auto, autoDet)
+        
+        # marque l'état marqué 
+        etatsMarque.append(etatsNonMarque[0])
+
+        # pour chaque état trouvé on l'ajoute dans les états non marqué 
+        for e in etatsTrouve:
+            if e not in etatsNonMarque and e not in etatsMarque:
+                etatsNonMarque.append(e)
+        
+        # on retire l'état marqué des étatsNonMarqué
+        etatsNonMarque.pop(0)
+        
+        
+    
+    return autoDet
+
+
+def marquer(etatsActif : list, auto, autoDet):
+    """
+    prend un état 
+    un auto non déterministe
+    et un auto déterministe
+    
+    on teste tout tes transistions de l'état actif
+    on regroupe tout les transi partant de etatActif
+
+    revoit tout les état où va les transitions
+    pour append les états non marqué
+    pas besoin de prendre l'auto determiste modif puisque c'est les modif 
+    seront accessibles dans la fonc determise
+    """
+
+    # liste des états trouvé 
+    etatsTrouve = list()
+
+    # toutes les transi partant de l'état actif
+
+    # liste de toutes les transi partant des états dans etatsActif
+    transi = list()
+
+    # pour chaque etat 
+    for etat in etatsActif:
+        transi.extend(list(filter(lambda transi : transi[0] == etat, auto['transitions'])))
     
 
+    # pour chaque lettre de l'alphabet
+    for a in auto['alphabet']:
+        
+        # liste des états qui sont 'pointé' par les transition d'étiquette a
+        etatsPointe = list(set(map(lambda transi: transi[2], filter(lambda transi: transi[1] == a, transi))))
 
+        # si il y a un ou plusieurs transi d'étiquette a
+        if len(etatsPointe) > 0:
 
+            # on ajoute la transition de etatsActif d'étiquette a et d'état pointé etatsPointe
+            autoDet['transitions'].append([etatsActif, a, etatsPointe])
+
+            if etatsPointe not in etatsTrouve:
+                etatsTrouve.append(etatsPointe)
+
+    
+    # on marque l'état en le mettant dans les états de l'auto det
+    autoDet['etats'].append(etatsActif)
+
+    # on ajoute l'état dans F si il contient un état F de l'auto original
+    
+    # si l'intersection n'est pas vide
+    if set(etatsActif).intersection(set(auto['F'])) != set(): 
+        autoDet['F'].append(etatsActif)
+
+    # on renvoit les état touvé pour les mettre dans étatsNonMarqué
+    return etatsTrouve
+    
+    
+def renommage(auto):
+    
+    # on utilise un dict en tant que table de correspondance
+    # sous la forme
+    # [ancient nom] : nouveau nom 
+
+    newName = dict()
+
+    for i, etat in enumerate(auto['etats']):
+        newName[tuple(etat)] = i
+    
+    auto['etats'] = [i for i in range(len(auto['etats']))]
+
+    # on modif les transi avec la table de correspondance newName
+    
+    # on la recontruit de 0
+    nTransiLst = list()
+
+    for transi in auto['transitions']:
+        nTransiLst.append([newName[tuple(transi[0])], transi[1], newName[tuple(transi[2])]])
+    auto['transitions'] = nTransiLst
+
+    # meme chose pour I 
+    auto['I'] = [newName[tuple(etatInit)] for etatInit in auto['I']]
+
+    # meme chose pour F
+    auto['F'] = [newName[tuple(etatFin)] for etatFin in auto['F']]
+
+    return auto
+        
 if __name__ == "__main__":
 
 
@@ -64,3 +191,7 @@ if __name__ == "__main__":
     auto1 ={"alphabet":['a','b'],"etats": [0,1], "transitions":[[0,'a',0],[0,'b',1],[1,'b',1],[1,'a',1]], "I":[0],"F":[1]}
     
     auto2={"alphabet":['a','b'],"etats": [0,1], "transitions":[[0,'a',0],[0,'a',1],[1,'b',1],[1,'a',1]], "I":[0],"F":[1]}   
+
+    print(determinise(auto2))
+
+    print(renommage(determinise(auto2)))
